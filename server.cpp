26f1d6e7a16258f1dd353c0d9f1ae925f591d4ad
weijingtao <625171422@qq.com>
2016-07-04 22:35:47 +0800

    add

--- server.cpp
+++ server.cpp
@@ -4,3 +4,41 @@
 
 #include "server.h"
 
+server::server()
+    : m_stop_flag(true),
+      m_thread_poll(2) {
+
+}
+
+server::~server() {
+    stop();
+}
+
+void server::start() {
+    m_stop_flag = false;
+    m_thread_poll.start(std::bind(&server_base::run, this));
+}
+
+void server::stop() {
+    m_stop_flag = true;
+}
+
+int server::init() {
+    return 0;
+}
+
+void server::run() {
+    SNOW_LOG_TRACE << "server runing" << std::endl;
+    m_proxy.set_pkg_spliter(std::bind(&server_base::pkg_check, this, std::placeholders::_1, std::placeholders::_2));
+    m_proxy.set_request_dispatcher(std::bind(&server_base::request_dispatch, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+    while(!m_stop_flag) {
+        m_ios.run();
+    }
+}
+
+void server::request_dispatch(const char* req_data, std::size_t req_len, response_dispatch_type rsp_dispatcher) {
+    SNOW_LOG_TRACE <<  "new request : " << req_data << std::endl;
+    auto new_session = std::make_shared<session_type>(m_ios);
+    new_session->set_response_dispatcher(std::move(rsp_dispatcher));
+    new_session->start(req_data, req_len);
+}
--- server.h
+++ server.h
@@ -21,30 +21,16 @@ namespace snow
     public:
         typedef std::function<void(const buffer&)>      response_dispatch_type;
 
-        server()
-            : m_stop_flag(true),
-              m_proxy(m_ios),
-              m_thread_poll(2) {
+        server();
 
-        }
+        virtual ~server();
 
-        virtual ~server() {
-            stop();
-        }
+        void start();
 
-        void start() {
-            m_stop_flag = false;
-            m_thread_poll.start(std::bind(&server_base::run, this));
-        }
-
-        void stop() {
-            m_stop_flag = true;
-        }
+        void stop();
 
     protected:
-        virtual int init() {
-            return 0;
-        }
+        virtual int init();
 
         virtual std::size_t pkg_check(const char* data, std::size_t len) = 0;
 
@@ -53,26 +39,13 @@ namespace snow
         server(const server&) = delete;
         void operator=(const server&) = delete;
 
-        void run() {
-            SNOW_LOG_TRACE << "server runing" << std::endl;
-            m_proxy.set_pkg_spliter(std::bind(&server_base::pkg_check, this, std::placeholders::_1, std::placeholders::_2));
-            m_proxy.set_request_dispatcher(std::bind(&server_base::request_dispatch, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
-            while(!m_stop_flag) {
-                m_ios.run();
-            }
-        }
+        void run();
 
-        void request_dispatch(const char* req_data, std::size_t req_len, response_dispatch_type rsp_dispatcher) {
-            SNOW_LOG_TRACE <<  "new request : " << req_data << std::endl;
-            auto new_session = std::make_shared<session_type>(m_ios);
-            new_session->set_response_dispatcher(std::move(rsp_dispatcher));
-            new_session->start(req_data, req_len);
-        }
+        void request_dispatch(const char* req_data, std::size_t req_len, response_dispatch_type rsp_dispatcher);
 
     protected:
         bool                    m_stop_flag;
         std::vector<acceptor>   m_acceptors;
-        proxy                   m_proxy;
         thread_poll             m_thread_poll;
     };
 }
